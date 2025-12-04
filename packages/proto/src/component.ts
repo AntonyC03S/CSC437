e597b4411591b1e282fb513d7df6b904f84b4ff9
Antony Chen <122947939+AntonyC03S@users.noreply.github.com>
2025-12-04 00:31:33 -0800

    .

---
+++ packages/proto/src/component.ts
@@ -0,0 +1,13 @@
+// src/destination.ts
+import { html, css, LitElement } from "lit";
+import { property } from "lit/decorators.js";
+
+export class DestinationElement extends LitElement {
+  override render() {
+    return html`
+      <!-- paste your template here -->
+    `;
+  }
+
+  static styles = css``;
+}
