b55c6488ca907b5d17249abcdc359d539180f382
Antony Chen <achen321@calpoly.edu>
2025-12-05 13:11:01 -0800

    Update

---
+++ node_modules/proto/public/js/cards.js
@@ -0,0 +1,16 @@
+/*
+https://codepen.io/Hyperplexed/pen/MWQeYLW
+Magical Hover Effect (w/ Tutorial)
+by Hyperplexed
+*/
+
+document.getElementById("cards").onmousemove = (e) => {
+  for (const card of document.getElementsByClassName("card")) {
+    const rect = card.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  }
+};
